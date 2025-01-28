cfd3547e9689f3d04282196dfa2aca6a7bb85ecb
C0dingGuru <a.b.alsamt@gmail.com>
2025-01-28 10:25:32 +0400

    Add files via upload

---
+++ 01_Variables/VariablesTask2.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x = 10; //declared and initialized the value of the variable
+
+    if x == 10 
+    {
+        println!("x is ten!");
+    } 
+    else 
+    {
+        println!("x is not ten!");
+    }
+}
---
+++ 01_Variables/VariablesTask4.rs
@@ -0,0 +1,8 @@
+// TODO: Fix the compiler error.
+fn main() 
+{
+    let mut x = 3; //add 'mut' 
+    println!("Number {x}");
+    x = 5; // Don't change this line
+    println!("Number {x}");
+}
